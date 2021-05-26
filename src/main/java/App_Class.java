b093d5b642c4373e50b1af2e843886c0fc3840b8
sankarg86 <sankarg86@gmail.com>
2021-05-26 17:45:27 +0100

    Jenkins test

---
+++ src/main/java/App_Class.java
@@ -0,0 +1,8 @@
+public class App_Class {
+
+    public void app(){
+        System.out.println("Test Jenkins");
+    }
+
+
+}
---
+++ src/test/java/Test_App.java
@@ -0,0 +1,10 @@
+import org.junit.jupiter.api.Test;
+
+public class Test_App {
+
+    @Test
+    public void test_app(){
+        System.out.println("Test case 1");
+    }
+
+}
