14ed84672d977e3bb87d2abbf021e82ad65076cd
sankarg86 <sankarg86@gmail.com>
2021-05-26 18:06:26 +0100

    Jenkins test

--- src/test/java/Test_App.java
+++ src/test/java/Test_App.java
@@ -1,3 +1,4 @@
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 public class Test_App {
@@ -10,6 +11,12 @@ public class Test_App {
     @Test
     public void test_app1(){
         System.out.println("Test case 2");
+    }
+
+    @Test
+    public void test_app2(){
+        System.out.println("Test case 2");
+        Assertions.fail();
     }
 
 }
